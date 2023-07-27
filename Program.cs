5d6b825c83f9f147d75ec1bd96c49649bdde9ceb
Victorcalp <victorcalp@outlook.com.br>
2023-07-27 08:26:36 -0300

    Criado CRUD

--- Program.cs
+++ Program.cs
@@ -1,7 +1,68 @@
-﻿class Program{
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+class Program{
     private const string Connection_String = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
 
     static void Main(string[] args){
-          
+        DeleteUser();
+        //UpdateUser();
+        ReadUsers();
+        //CreateUser();   
+    }
+    public static void ReadUsers(){
+        using var connection = new SqlConnection(Connection_String);
+        var users = connection.GetAll<User>(); //lista todos os users da base
+
+        foreach (var user in users)
+        {
+            Console.WriteLine(user.Name);
+        }
+    }
+    public static void ReadUser(){
+        using var connection = new SqlConnection(Connection_String);
+        var user = connection.Get<User>(1); //lista todos os users da base
+
+        Console.WriteLine(user.Name);
+    }
+    public static void CreateUser(){
+        var user = new User()
+            {
+                Name = "Equipe balta.io",
+                Bio = "Equipe balta.io",
+                Image = "https://...",
+                Email = "hello@balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe.balta",
+            };
+
+        using var connection = new SqlConnection(Connection_String);
+        connection.Insert<User>(user);
+        Console.WriteLine("Cadastro realizado com sucesso");
+    }
+    public static void UpdateUser(){
+        var user = new User()
+            {
+                Id = 2,
+                Name = "Equipe do balta.io",
+                Bio = "Equipe balta.io",
+                Image = "https://...",
+                Email = "hello@balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe.balta",
+            };
+
+        using var connection = new SqlConnection(Connection_String);
+        connection.Update<User>(user);
+        Console.WriteLine("Update realizado com sucesso");
+    }
+
+    public static void DeleteUser(){
+
+        using var connection = new SqlConnection(Connection_String);
+        var user = connection.Get<User>(2);
+        connection.Delete<User>(user);
+        Console.WriteLine("Exclusão realizada");
     }
 }
