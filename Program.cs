0efac0852bb139139809f1131146973b96c4645b
Victorcalp <victorcalp@outlook.com.br>
2023-07-27 08:26:03 -0300

    Criado conexão com BD

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿class Program{
+    private const string Connection_String = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
+
+    static void Main(string[] args){
+          
+    }
+}
