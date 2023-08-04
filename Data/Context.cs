1a31718c07ba95da50152435115d58fdd3afd4be
Victorcalp <victorcalp@outlook.com.br>
2023-08-04 01:15:29 -0300

    Utilizando migrations

--- Data/Context.cs
+++ Data/Context.cs
@@ -19,7 +19,7 @@ namespace Blog.Data
         //Realiza a conexão com BD
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;");
+            options.UseSqlServer("Server=localhost,1433;Database=BlogMigrations;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;");
             // options.LogTo();
         }
 
